e6f8a3e8df0a801efe9497df0a5e91b698646a3c
Carlyees <carlyees@example.com>
2026-02-02 22:08:39 +0500

    Updating and deleting tasks in the database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi import Depends
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.database import engine
 from app import models
+from app.models import Task
+from app.schemas import TaskCreate, TaskResponse, TaskUpdate
 
 def get_db():
     db = SessionLocal()
@@ -17,14 +20,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 @app.get("/")
-
 def root():
     return {"message": "Task Planner API is running"}
-
-from app.schemas import TaskCreate, TaskResponse
-from app.models import Task
-
-tasks = []
 
 @app.get("/tasks", response_model=list[TaskResponse])
 def get_tasks(db: Session = Depends(get_db)):
@@ -37,3 +34,39 @@ def create_task(task: TaskCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_task)
     return db_task
+
+@app.put("/tasks/{task_id}", response_model=TaskResponse)
+def update_task(
+    task_id: int,
+    task: TaskUpdate,
+    db: Session = Depends(get_db)
+):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+
+    if db_task is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found"
+        )
+        
+    db_task.completed = task.completed
+    db.commit
+    db.refresh(db_task)
+    return db_task
+
+@app.delete("/task/{task_id}")
+def delete_task(
+    task_id: int,
+    db: Session = Depends(get_db)
+):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+
+    if db_task is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found"
+        )
+    
+    db.delete(db_task)
+    db.commit
+    return {"message": "Task deleted"}
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,9 +1,17 @@
 from pydantic import BaseModel
 
-class TaskCreate(BaseModel):
+class TaskBase(BaseModel):
     title: str
 
-class TaskResponse(BaseModel):
-    id: int
-    title: str
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(BaseModel):
     completed: bool
+
+class TaskResponse(TaskBase):
+    id: int
+    completed: bool
+
+    class Config:
+        orm_mode = True
